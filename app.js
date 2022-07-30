dfd31d1231354d09b764c2e8a11e37b9f0fa6924
Ashu Sharma <ashusharma0771@gmail.com>
2022-07-30 20:19:06 +0530

    intial node app setup complete

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-//jshint esversion:6
+import express from "express";
+import bodyParser from "body-parser";
+
+const port = 3000;
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Application running on port ${port}.`);
+});
