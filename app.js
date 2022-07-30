3f57a7715e122fcd92fa71f70229a20ea87bb977
Ashu Sharma <ashusharma0771@gmail.com>
2022-07-30 20:27:51 +0530

    add root, login and register routes to app.js

--- app.js
+++ app.js
@@ -9,7 +9,15 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
 });
 
 app.listen(port, () => {
