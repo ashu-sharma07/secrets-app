a2b60409b43f20de0384b2708a4f89be3735b440
Ashu Sharma <ashusharma0771@gmail.com>
2022-08-02 22:01:38 +0530

    add post route for register

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 import express from "express";
 import bodyParser from "body-parser";
+import mongoose from "mongoose";
 
 const port = 3000;
 const app = express();
 
+// Basic App Set up
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
+
+// Mongoose Database
+const URL = "mongodb://localhost:27017/userDB";
+mongoose.connect(URL);
+
+const userSchema = {
+  email: String,
+  password: String,
+};
+
+const User = new mongoose.model("User", userSchema);
 
 app.get("/", (req, res) => {
   res.render("home");
@@ -19,6 +32,24 @@ app.get("/login", (req, res) => {
 app.get("/register", (req, res) => {
   res.render("register");
 });
+
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.get("/logout", (req, res)=>{
+  res.redirect("/");
+})
 
 app.listen(port, () => {
   console.log(`Application running on port ${port}.`);
